4f05dac530ca74c5c4c3e17f42220c1f678e5009
f100cleveland <dustin.rinne@gmail.com>
2016-01-11 15:11:37 -0600

    Update and rename vrtoxin.mk to tipsy.mk

--- tipsy.mk
+++ tipsy.mk
@@ -1,11 +1,11 @@
 # Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
-# Inherit some common Toxic stuff.
-$(call inherit-product, vendor/vrtoxin/config/common.mk)
+# Inherit some common Tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common.mk)
 
 # Setup device specific product configuration.
-PRODUCT_NAME := vrtoxin_angler
+PRODUCT_NAME := tipsy_angler
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := angler
 PRODUCT_MODEL := Nexus 6P
@@ -15,7 +15,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     BUILD_FINGERPRINT="google/angler/angler:6.0.1/MMB29P/2473553:user/release-keys" \
     PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29P 2473553 release-keys"
-
-# Boot animation
-PRODUCT_COPY_FILES += \
-    vendor/vrtoxin/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip

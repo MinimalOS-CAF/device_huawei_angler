4a9c072ccf0ed50092a19348b877e01eedb4241a
Aldrin Holmes (CallMeAldy) <hello@aldrinholmes.com>
2016-02-20 09:15:03 +0100

    Revert "Use CAF hardware"
    This reverts commit 1c069b24318634fe312ff06e64ad3648731996dd.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,5 +146,12 @@ TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := angler_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
-# Qualcomm support
-BOARD_USES_QCOM_HARDWARE := true
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
